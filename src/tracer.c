89fa8ba0ae1a3feb9a829cb24c41116d28dcf0d2
Mariana <a100702@alunos.uminho.pt>
2023-04-25 13:30:23 +0100

    parte do tempo feita

--- src/tracer.c
+++ src/tracer.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <sys/time.h>
 
 struct Info { //se o programName for NULL então é final
     int pid;
@@ -17,13 +18,13 @@ struct String {
     char *content;
 };
 
-struct String *to_String(struct Info info)
+struct String *to_String(struct Info info, char *time_string)
 {
    struct String *result = malloc(sizeof(struct String));
-    result->lenght = snprintf(NULL, 0, "PID: %d NAME: %s \n", info.pid, info.programName) + 1;
+    result->lenght = snprintf(NULL, 0, "PID: %d NAME: %s TIME: %s \n", info.pid, info.programName, time_string) + 1;
    result->content = malloc(result->lenght);
    if(result->content == NULL) return NULL;
-   snprintf(result->content, result->lenght, "PID: %d NAME: %s \n", info.pid, info.programName);
+   snprintf(result->content, result->lenght, "PID: %d NAME: %s TIME: %s \n", info.pid, info.programName, time_string);
    return result;
 }
 
@@ -51,11 +52,15 @@ int main(int argc, char **argv){
             }
 
 //--------------------Notificar cliente----------------------------
-              int fd;
+            int fd;
             pid_t a;
-            double time_execute;
-            clock_t start_time_filho, start_time_pai, end_time;
+            int time_execute;
             int pipe_time[2];
+            struct timeval current_time_filho;
+            struct timeval current_time_pai;
+            struct tm *nowtm;
+            char *time_string = (char*)malloc(sizeof(char)* 32);
+            char *time_ms = (char*)malloc(sizeof(char) * 8);
 
             if(pipe(pipe_time) == -1){
                 perror("erroPipe");
@@ -63,7 +68,7 @@ int main(int argc, char **argv){
             }
 
             if((a = fork()) == 0){
-                close(pipe_time[0]);
+ 
                 struct Info inicial;
                 inicial.pid = getpid();
                 inicial.programName = strdup(argv[3]);
@@ -76,13 +81,21 @@ int main(int argc, char **argv){
                 if((fd = open("fifo",O_WRONLY)) == -1)
                     return 2;
                 // mandar a informação para o server
-                printf("\nprogram name: %s|\n",inicial.programName);
+                printf("\nProgram name: %s|\n",inicial.programName);
                 
-                start_time_filho = clock();
-                write(pipe_time[1], &start_time_filho, sizeof(double));
-                close(pipe_time[1]);
+                        gettimeofday(&current_time_filho, NULL);
+                        time_t nowtime = current_time_filho.tv_sec;
+                        nowtm = localtime(&nowtime);
 
-                message = to_String(inicial);
+                        strftime(time_string, 32, "%Y-%m-%d %H:%M:%S", nowtm); 
+                        sprintf(time_ms, ".%ld", current_time_filho.tv_usec);
+                        strcat(time_string, time_ms);
+
+                        close(pipe_time[0]);
+                        write(pipe_time[1], &current_time_filho.tv_usec, sizeof(suseconds_t));
+                        close(pipe_time[1]);
+
+                message = to_String(inicial, time_string);
                 write (fd, message->content,sizeof(char) * message->lenght);
                 close (fd);
 
@@ -92,14 +105,22 @@ int main(int argc, char **argv){
                 exit(0);
             }
             wait(0);
-            end_time = clock();
 
-            close(pipe_time[1]);
-            read(pipe_time[0], &start_time_pai, sizeof(double));
-            close(pipe_time[0]);
+                        gettimeofday(&current_time_pai, NULL);
+                        time_t nowtime = current_time_pai.tv_sec;
+                        nowtm = localtime(&nowtime);
 
-            time_execute = ((double)(end_time-start_time_pai) * 1000) / CLOCKS_PER_SEC;
-            printf("Ended in %fms\n", time_execute);
+                        strftime(time_string, 32,"%Y-%m-%d %H:%M:%S", nowtm);
+                        sprintf(time_ms, ".%ld", current_time_pai.tv_usec);
+                        strcat(time_string, time_ms);
+
+
+                        close(pipe_time[1]);
+                        read(pipe_time[0], &current_time_filho.tv_usec, sizeof(suseconds_t));
+                        close(pipe_time[0]);
+
+                        time_execute = current_time_pai.tv_usec - current_time_filho.tv_usec;
+            printf("Ended in %dms\n", time_execute);
 //-----------------------------------------------------------------
 //--------------------Notificar servidor---------------------------
             
@@ -111,7 +132,7 @@ int main(int argc, char **argv){
             final.pid = a;
             
             final.programName = strdup("Ended");
-            message = to_String(final);
+            message = to_String(final, time_string);
 
             write (fd, message->content,sizeof(char) * message->lenght);
             close (fd);
@@ -131,4 +152,3 @@ int main(int argc, char **argv){
         return 0;
     }
 }
-
