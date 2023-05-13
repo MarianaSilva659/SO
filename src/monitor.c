fd987e9d10e9f1675c21b7876e34ba032e83a945
jpcg27 <a100747@alunos.uminho.pt>
2023-05-13 17:13:20 +0100

    meti um write em comentário

--- src/monitor.c
+++ src/monitor.c
@@ -125,7 +125,7 @@ int main(int argc, char **argv){
             default:
             break;
             }
-            write(1,buffer,strlen(buffer));
+          //  write(1,buffer,strlen(buffer));
             i +=2;
           }
            // printf("%s \n", buffer);
